095514c1658e531d60522f157b389d1fb0d529db
yuzich <olga@yuzich.ru>
2022-04-24 00:32:10 +0700

    добавила в url ссылку и вевела сокращенную

--- js/shorten-link.js
+++ js/shorten-link.js
@@ -3,26 +3,24 @@ let buttonShorten = document.querySelector('.button-shorten');
 
 
 buttonShorten.addEventListener('click', () => {
-    let longLink = document.querySelector('.longLink').value;
-
+    const longLink = document.querySelector('.longLink').value;
     async function sendData() {
-        let response = await fetch('https://api.shrtco.de/v2/', {
-            method: 'POST',
-            body: JSON.stringify(longLink)
-          });
+        let response = await fetch(`https://api.shrtco.de/v2/shorten?url=${longLink}`);
 
           if(!response.ok) {
               alert('да хер там');
           }
 
-        let result = await response.text();
-        alert(result.message);
+        let result = await response.json();
+          // result - текст, у него нет свойства message
+        //alert(result.message);
+        alert(result.result.short_link);
     }
 
     sendData();
 
 
-    
+
     // async function getData() {
     //     let response = await fetch('https://api.shrtco.de/v2/info?code=example');
     //     let link = await response.text();
@@ -31,4 +29,3 @@ buttonShorten.addEventListener('click', () => {
 
     // getData();
 })
-  
