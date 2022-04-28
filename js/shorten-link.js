e60ce7aedf557c2b568541831dd7ce08a4b6944c
Kirill Tepljakov <super.css2017@yandex.ru>
2022-04-28 13:37:58 +0300

    сделал сортировку по новым ссылкам

--- js/shorten-link.js
+++ js/shorten-link.js
@@ -1,40 +1,53 @@
 const buttonShorten = document.querySelector('.button-shorten');
-const blockLinkContainer = document.querySelector('.block-link_container');
+let blockLinkContainer = document.querySelector('.block-link_container');
+let arrayChildren = [];
 
 
 buttonShorten.addEventListener('click', () => {
-    let children = document.querySelector('.block-link_container').children.length;
-    
-    if(children <= 3) {
-       const newBlockLinkContainer = document.querySelector('.wrap-block-link_container');
-       const newBlockLink = newBlockLinkContainer.cloneNode(true);
-       blockLinkContainer.appendChild(newBlockLink);
-    }
 
-    let longLink = document.querySelector('.longLink').value;
-    const newBlock = document.querySelector('.wrap-block-link_container');
-    const shortenLink = document.querySelector('.shorten-link');
-
-    async function sendData() {
-        let response = await fetch(`https://api.shrtco.de/v2/shorten?url=${longLink}`);
-        let startLink = document.querySelector('.start-link');
-
-        if(!response.ok) {
-            alert('Не корректная ссылка');
-        }
-
-        let result = await response.json();
-
-        if(result) {
-            newBlock.style.display = 'block';
-            startLink.textContent = longLink;
-            shortenLink.textContent = result.result.short_link;
-            document.querySelector('.longLink').value = '';
-        }
+    function createLink() {
+        let children = document.querySelector('.block-link_container').children.length;
+        arrayChildren.push(children);
         
+        const newBlockLinkContainer = document.querySelector('.wrap-block-link_container');
+        const newBlockLink = newBlockLinkContainer.cloneNode(true);
+
+        blockLinkContainer.appendChild(newBlockLink);
+        
+        let longLink = document.querySelector('.longLink').value;
+        const newBlock = document.querySelector('.wrap-block-link_container');
+        const shortenLink = document.querySelector('.shorten-link');
+
+        async function sendData() {
+            let response = await fetch(`https://api.shrtco.de/v2/shorten?url=${longLink}`);
+            let startLink = document.querySelector('.start-link');
+
+            if(!response.ok) {
+                alert('Не корректная ссылка');
+            }
+
+            let result = await response.json();
+
+            if(result) {
+                newBlock.style.display = 'block';
+                startLink.textContent = longLink;
+                shortenLink.textContent = result.result.short_link;
+                document.querySelector('.longLink').value = '';
+            } 
+        }
+        sendData();
     }
 
-    sendData();
-    
+    function deleteLastLink() {
+        blockLinkContainer.removeChild(blockLinkContainer.lastChild);
+        arrayChildren.shift();
+    }
+
+    if(arrayChildren.length < 3) {
+        createLink();
+    } else {
+        deleteLastLink();
+        createLink();
+    }
 })
   
